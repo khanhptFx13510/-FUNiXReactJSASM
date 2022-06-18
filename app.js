2f1b2d4aa2b43c26d0bf184b1d5334b3b475e1e1
khanhptFx13510 <khanhdudn@gmail.com>
2022-06-18 16:48:47 +0700

    chuyen huong request

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-   console.log(req.url, req.method, req.headers);
+   
+   const url = req.url;
+   if(url === '/'){
+      res.write('<html>');
+      res.write('<head><title>Enter message</title></head>');
+      res.write('<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">send</button></form></body>');
+      res.write('</html>');
+      return res.end();
+   }
+
    res.setHeader('Content-Type', 'text/html');
    res.write('<html>');
    res.write('<head><title>My First Page</title></head>');
