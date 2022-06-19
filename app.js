1e7488d5e907a3617bd4d6481d7eb6956899428e
khanhptFx13510 <khanhdudn@gmail.com>
2022-06-19 10:58:03 +0700

    chuyen huong request 2

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-   
+const server = http.createServer((req, res) => {  
    const url = req.url;
+   const method = req.method;
+   console.log("abc");
    if(url === '/'){
       res.write('<html>');
       res.write('<head><title>Enter message</title></head>');
-      res.write('<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">send</button></form></body>');
+      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>');
       res.write('</html>');
+      return res.end();
+   }
+   
+   if(url === 'message' && method === 'POST'){
+      fs.writeFileSync('message.txt' , 'DUMMY');
+      res.statusCode= 302;
+      res.setHeader('Location', '/');
       return res.end();
    }
 
