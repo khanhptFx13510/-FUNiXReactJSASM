e7dfb6c93bb1d62f729eb881c87b5d91e7cd5854
khanhptFx13510 <khanhdudn@gmail.com>
2022-07-25 10:02:51 +0700

    xu ly route khac nhau

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const http = require('http');
-
+// const http = require('http');
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-   console.log('In the middleware');
+app.use('/' , (req, res, next) => {
+   console.log('this always run');
    next();
+})
+
+app.use('/add-product',(req, res, next) => {
+   console.log('In the middleware');
+   res.send('the "add product" page');
 })
 
 app.use((req , res , next) => {
@@ -14,6 +18,7 @@ app.use((req , res , next) => {
    res.send("<h1>Hello from Express</h1>");
 })
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000);
+// server.listen(3000);
+app.listen(3000);
