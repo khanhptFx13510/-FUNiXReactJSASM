a1047b14dda655de48890c119d07446b17894ee4
khanhptFx13510 <khanhdudn@gmail.com>
2022-07-25 11:47:11 +0700

    filtering paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => Get
 router.get('/add-product',(req, res, next) => {
-   res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">add product</button></form>');
+   res.send('<form action="/admin/product" method="POST"><input type="text" name="title"/><button type="submit">add product</button></form>');
 })
 
-router.post('/product' , (req , res , next) => {
+// /admin/add-product => POST
+router.post('/add-product' , (req , res , next) => {
    console.log(req.body);
    res.redirect('/');
 })
