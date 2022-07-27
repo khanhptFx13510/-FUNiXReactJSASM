edce910aba1f18ceba1441ca3d4866fbcbf9d46d
khanhptFx13510 <khanhdudn@gmail.com>
2022-07-27 09:31:54 +0700

    serving html page

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 // /admin/add-product => Get
 router.get('/add-product',(req, res, next) => {
-   res.send('<form action="/admin/product" method="POST"><input type="text" name="title"/><button type="submit">add product</button></form>');
+   res.sendFile(path.join(__dirname, '../' , 'views', 'add-product.html'));
 })
 
 // /admin/add-product => POST
