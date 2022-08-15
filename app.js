cc27614803aa9c2e398272608a0b897cb56f9402
khanhptFx13510 <khanhdudn@gmail.com>
2022-08-15 11:08:40 +0700

    working on layout with partials

--- app.js
+++ app.js
@@ -2,15 +2,14 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views' , 'views');
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-   res.status(404).render('404', { pageTitle: 'Page Not Found' });
+   res.status(404).render('404', { pageTitle: 'Page Not Found', path: '404' });
 })
 // const server = http.createServer(app);
 
