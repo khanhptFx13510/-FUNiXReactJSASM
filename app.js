d9b6d0daf1fea44142b45adf2426fe0bc030d72d
khanhptFx13510 <khanhdudn@gmail.com>
2022-08-16 10:06:31 +0700

    Finish Controller

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const errorController = require('./controllers/error.js');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,9 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-   res.status(404).render('404', { pageTitle: 'Page Not Found', path: '404' });
-})
+app.use(errorController.get404);
 // const server = http.createServer(app);
 
 // server.listen(3000);
