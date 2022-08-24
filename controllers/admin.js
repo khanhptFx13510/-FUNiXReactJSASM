0e114bf5bf016a46e3a95e8d7f65762e1cbc3084
khanhptFx13510 <khanhdudn@gmail.com>
2022-08-24 10:30:20 +0700

    inserting data & creating a product

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,18 +10,22 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
    const title = req.body.title;
-   const imageUrl = req.body.imageUrl;
    const price = req.body.price;
+   const imageUrl = req.body.imageUrl;
    const description = req.body.description;
-   const product = new Product(null ,title, imageUrl, description, price);
-   product
-      .save()
-      .then(() => {
-         res.redirect('/');
+   Product.create({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+   })
+      .then(result => {
+         // console.log(result);
+         console.log("create a product");
       })
-      .catch((err) => console.log(err));
-   res.redirect('/');
+      .catch(err => {console.log(err)});
 };
+   
 
 exports.getEditProduct = (req, res, next) => {
    const editMode = req.query.edit;
