9a56b4454f8722838d8e081cba080d49d1fc89d3
khanhptFx13510 <khanhdudn@gmail.com>
2022-08-25 11:23:27 +0700

    bug------

--- controllers/shop.js
+++ controllers/shop.js
@@ -116,38 +116,48 @@ exports.postCartDeleteProduct =(req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
+   let fetchedCart;
    req.user
       .getCart()
-      .then(cart => {
-         return cart.getProducts()
+      .then((cart) => {
+         fetchedCart = cart;
+         return cart.getProducts();
       })
-      .then(products => {
+      .then((products) => {
          return req.user
             .createOrder()
-            .then(order => {
-               return order.addProducts(products.map(product =>{
-                  product.orderItem = { quantity: product.cartItem.quantity};
-                  return product;
-               }));
+            .then((order) => {
+                  return order.addProducts(
+                     products.map((product) => {
+                        product.orderItem = {
+                              quantity: product.cartItem.quantity,
+                        };
+                        return product;
+                     })
+                  );
             })
-            .catch(err => console.log(err))
+            .catch((err) => console.log(err));
       })
-      .then(result =>{
+      .then((result) => {
+         return fetchedCart.setProducts(null);
+      })
+      .then((result) => {
          res.redirect('/orders');
       })
-      .catch(err => console.log(err));
-}
-
-exports.getOrders = (req, res, next) => {
-   res.render('shop/orders', {
-      path: '/orders',
-      pageTitle: 'Your Orders',
-   });
+      .catch((err) => console.log(err));
 };
 
-exports.getCheckout = (req, res, next) => {
-   res.render('shop/checkout', {
-      path: '/checkout',
-      pageTitle: 'Checkout',
-   });
+exports.getOrders = (req, res, next) => {
+   req.user
+      .getOrders({ include: ['products'] })
+      .then((orders) => {
+         res.render('shop/orders', {
+            path: '/orders',
+            pageTitle: 'Your Orders',
+            orders: orders,
+         });
+      })
+      .catch((err) => {
+         console.log(err);
+      });
 };
