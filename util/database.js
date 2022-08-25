579b82f51d634c84ec655514dd5c54c4d4351d55
khanhptFx13510 <khanhdudn@gmail.com>
2022-08-25 16:41:30 +0700

    lab06_1

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root' , '043431', {
-   dialect: 'mysql', 
-   host: 'localhost'
-});
+const mogoConnect = (callback) =>{
+   MongoClient.connect('mongodb+srv://khanhpham:khanhdu123@khanhpham.6zl1ibi.mongodb.net/?retryWrites=true&w=majority')
+      .then(client => {
+         console.log('conneted!');
+         callback(client);
+      })
+      .catch(err => console.log(err))
+}
 
-module.exports = sequelize;
+module.exports = mogoConnect
