49378cfc1ca89ca26e6eaf46d08d16701c3f9a3e
khanhptFx13510 <khanhdudn@gmail.com>
2022-08-26 12:06:15 +0700

    lab06_12

--- controllers/admin.js
+++ controllers/admin.js
@@ -75,15 +75,12 @@ exports.getProducts = (req, res, next) => {
       });
 };    
 
-// exports.postDeleteProduct = (req, res, next) => {
-//    const prodId = req.body.productId;
-//    Product.findByPk(prodId)
-//       .then(product =>{
-//          return product.destroy();
-//       })
-//       .then(result => {
-//          console.log("deleted product");
-//          res.redirect('/admin/products');
-//       })
-//       .catch(err => console.log(err));
-// };
+exports.postDeleteProduct = (req, res, next) => {
+   const prodId = req.body.productId;
+   Product.deleteById(prodId)
+      .then(result => {
+         console.log("deleted product");
+         res.redirect('/admin/products');
+      })
+      .catch(err => console.log(err));
+};
