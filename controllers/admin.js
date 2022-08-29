1ae9147225ad9df896baa95be37bb5759ccaa70a
khanhptFx13510 <khanhdudn@gmail.com>
2022-08-29 11:32:10 +0700

    lab07_6

--- controllers/admin.js
+++ controllers/admin.js
@@ -36,7 +36,6 @@ exports.getEditProduct = (req, res, next) => {
    }
    const prodId = req.params.productId;
    Product.findById(prodId)
-   // Product.findByPk(prodId)
       .then(product => {
          if (!product) {
             return res.redirect('/');
@@ -57,8 +56,14 @@ exports.postEditProduct = (req, res, next) => {
    const updatedPrice = req.body.price;
    const updatedImageUrl = req.body.imageUrl;
    const updatedDesc = req.body.description;
-   const product = new Product(updatedTitle ,updatedPrice, updatedDesc, updatedImageUrl , prodId);
-   product.save()
+   Product.findById(prodId)
+      .then((product) => {
+         product.title = updatedTitle;
+         product.price = updatedPrice;
+         product.description = updatedDesc;
+         product.imageUrl = updatedImageUrl;
+         return product.save();
+      })
       .then(result => {
          console.log("Updated product");
          res.redirect('/admin/products');
@@ -67,7 +72,7 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-   Product.fetchAll()
+   Product.find()
       .then((products) => {
          res.render('admin/products', {
             prods: products,
