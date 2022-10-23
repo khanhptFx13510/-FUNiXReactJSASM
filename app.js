4b066dd9062b58af1f505a0fa50047319f4f4f05
khanhptFx13510 <khanhdudn@gmail.com>
2022-10-23 14:09:22 +0700

    lab08_5

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 // const mongoConnect = require('./util/database').mongoConnect;
@@ -19,7 +20,9 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(
+    session({ secret: 'my secret' , resave: false, saveUninitialized: false })
+);
 // fetch dummy user and store that in request 
 // so that we can use it for the rest of that request, in all the routes and controllers
 app.use((req, res, next) => {
