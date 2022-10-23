34b6bdc5e5d325fbda92ad85709d511c1bd65b8e
khanhptFx13510 <khanhdudn@gmail.com>
2022-10-23 19:39:13 +0700

    lab08_12

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,18 @@
 const User = require('../models/user');
 
-exports.getLogin = (req, res, next) => {
-   
+exports.getLogin = (req, res, next) => { 
    res.render('auth/login', {
        path: '/login',
        pageTitle: 'Login',
        isAuthenticated: false
+   });
+};
+
+exports.getSignup = (req, res, next) => {
+   res.render('auth/signup', {
+       path: '/signup',
+       pageTitle: 'Signup',
+       isAuthenticated: false,
    });
 };
 
@@ -22,6 +29,27 @@ exports.postLogin = (req, res, next) => {
       .catch((err) => console.log(err));
 };
 
+exports.postSignup = (req, res, next) => {
+   const email = req.body.email;
+   const password = req.body.password;
+   const confirmPassword = req.body.confirmPassword;
+   User.findOne({ email: email })
+      .then((userDoc) => {
+         if (userDoc) {
+            return res.redirect('/signup');
+         }
+         const user = new User({
+            email: email,
+            password: password,
+            cart: { items: [] },
+         });
+         return user.save();
+      })
+      .then((result) => {
+         res.redirect('/login');
+      })
+      .catch((err) => console.log(err));
+};
 exports.postLogout = (req, res, next) => {
    req.session.destroy((err) => {
       console.log(err);
