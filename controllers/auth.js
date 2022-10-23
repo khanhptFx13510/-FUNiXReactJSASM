4b471fbf20612a24f604a2a96e899d3d40b012ed
khanhptFx13510 <khanhdudn@gmail.com>
2022-10-23 14:24:03 +0700

    lab08_6

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,6 +9,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-   res.setHeader('Set-Cookie','loggedIn=true; Secure');
+   req.session.isLoggedIn = true;
    res.redirect('/');
 };
