252e5d9b620e739d41680f36827f0b228efc2f88
khanhptFx13510 <khanhdudn@gmail.com>
2022-10-23 15:26:07 +0700

    lab08_7

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 // const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://khanhpham:khanhdu123@khanhpham.6zl1ibi.mongodb.net/shop';
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,7 +27,12 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-    session({ secret: 'my secret' , resave: false, saveUninitialized: false })
+    session({ 
+        secret: 'my secret' , 
+        resave: false, 
+        saveUninitialized: false , 
+        store: store 
+    })
 );
 // fetch dummy user and store that in request 
 // so that we can use it for the rest of that request, in all the routes and controllers
@@ -42,7 +53,10 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://khanhpham:khanhdu123@khanhpham.6zl1ibi.mongodb.net/shop?retryWrites=true&w=majority')
+mongoose.connect(
+    MONGODB_URI
+    // 'mongodb+srv://khanhpham:khanhdu123@khanhpham.6zl1ibi.mongodb.net/shop?retryWrites=true&w=majority'
+)
 .then(result => {
     User.findOne().then((user) => {
         if (!user) {
