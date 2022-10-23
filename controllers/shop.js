85278e01b55ac9fc66903f5e61038f3b8f82a999
khanhptFx13510 <khanhdudn@gmail.com>
2022-10-23 23:18:05 +0700

    lab08_21

--- controllers/shop.js
+++ controllers/shop.js
@@ -89,7 +89,10 @@ exports.postOrder = (req, res, next) => {
             return { quantity: i.quantity, product: {...i.productId._doc} };
          });
          const order = new Order({
-            user: { name: req.user.name, userId: req.user },
+            user: { 
+               email: req.user.email, 
+               userId: req.user 
+            },
             products: products,
          });
          return order.save();
