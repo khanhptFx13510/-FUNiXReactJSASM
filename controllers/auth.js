eccdc5c669bbfb7747901280f9f97ee25742dcf0
khanhptFx13510 <khanhdudn@gmail.com>
2022-10-23 20:52:21 +0700

    lab08_15

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,16 +18,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-   User.findById('630c44f0675163f93d6851c2')
+   const email = req.body.email;
+   const password = req.body.password;
+   User.findOne({ email: email })
       .then((user) => {
-         req.session.isLoggedIn = true;
-         req.session.user = user;
-         req.session.save((err) => {
-            console.log(err);
-            res.redirect('/');
-         });
+         if (!user) {
+            return res.redirect('/login');
+         }
+         bcrypt
+         .compare(password, user.password)
+         .then((doMatch) => {
+            if (doMatch) {
+               req.session.isLoggedIn = true;
+               req.session.user = user;
+               return req.session.save((err) => {
+                  console.log(err);
+                  res.redirect('/');
+               });
+            }
+            res.redirect('/login');
+         })
       })
-      .catch((err) => console.log(err));
+      .catch((err) => 
+         console.log(err)
+      );
 };
 
 exports.postSignup = (req, res, next) => {
@@ -44,7 +58,7 @@ exports.postSignup = (req, res, next) => {
          .then((hashedPassword) => {
             const user = new User({
                email: email,
-               password: password,
+               password: hashedPassword,
                cart: { items: [] },
             });
             return user.save();
