ebfcd545094b18de94631ed89e60234897c1a7ca
khanhptFx13510 <khanhdudn@gmail.com>
2022-11-08 17:11:33 +0700

    lab09_18

--- app.js
+++ app.js
@@ -45,6 +45,12 @@ app.use(flash());
 app.use(express.json());
 
 app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
+app.use((req, res, next) => {
     if (!req.session.user) {
         return next();
     }
@@ -57,15 +63,10 @@ app.use((req, res, next) => {
             next();
         })
         .catch((err) => {
-            throw new Error(err);
+            next(new Error(err));
         });
 });
 
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
-});
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -82,7 +83,11 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
     // res.status(error.httpStatusCode).render(...)
-    res.redirect('/500');
+    res.status(500).render('500', {
+        pageTitle: 'Error!',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn,
+    });
 });
 mongoose.connect(
     MONGODB_URI
